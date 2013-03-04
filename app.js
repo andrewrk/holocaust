d1469b5b527fec41654fd98a8a712da748cd0c5a
Andrew Kelley <superjoe30@gmail.com>
2013-03-03 20:14:15 -0500

    basic perlin noise terrain

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+(function() {
 var canvas = document.getElementById("game");
 canvas.width = 1067;
 canvas.height = 600;
@@ -18,25 +19,98 @@ var requestAnimationFrame = window.requestAnimationFrame ||
 canvas.tabIndex = 0;
 var context = canvas.getContext('2d');
 var fps = targetFps;
+var landType = {
+  safe: {
+    name: "Safe",
+    color: '#48C13C',
+    weight: 0.25,
+  },
+  fatal: {
+    name: "Fatal",
+    color: '#860600',
+    weight: 0.25,
+  },
+  danger: {
+    name: "Danger",
+    color: '#F30B00',
+    weight: 0.36,
+  },
+  cleanWater: {
+    name: "Clean Water",
+    color: '#548FC4',
+    weight: 0.06,
+  },
+  contaminatedWater: {
+    name: "Contaminated Water",
+    color: '#6D2A49',
+    weight: 0.08,
+  },
+};
 
 var cellWidth = 6;
 var cellHeight = 6;
 var gridWidth = Math.floor(canvas.width / cellWidth);
 var gridHeight = Math.floor(canvas.height / cellHeight);
-var grid = new Array(gridHeight);
-var colors = [
-  '#48C13C', // safe
-  '#860600', // fatal
-  '#F30B00', // danger
-  '#548FC4', // clean water
-  '#6D2A49', // contaminated water
+canvas.width = gridWidth * cellWidth;
+canvas.height = gridHeight * cellHeight;
+var perlinNoise = generatePerlinNoise(gridWidth, gridHeight);
+
+var terrainThresholds = [
+  {
+    terrain: landType.fatal,
+    threshold: 0.15,
+  },
+  {
+    terrain: landType.contaminatedWater,
+    threshold: 0.23,
+  },
+  {
+    terrain: landType.danger,
+    threshold: 0.35,
+  },
+  {
+    terrain: landType.cleanWater,
+    threshold: 0.38,
+  },
+  {
+    terrain: landType.safe,
+    threshold: 0.52,
+  },
+  {
+    terrain: landType.danger,
+    threshold: 0.66,
+  },
+  {
+    terrain: landType.cleanWater,
+    threshold: 0.69,
+  },
+  {
+    terrain: landType.safe,
+    threshold: 0.80,
+  },
+  {
+    terrain: landType.danger,
+    threshold: 0.90,
+  },
+  {
+    terrain: landType.fatal,
+    threshold: 1.00,
+  },
 ];
 
+var grid = createArray(gridWidth, gridHeight);
 for (var y = 0; y < gridHeight; ++y) {
-  var row;
-  grid[y] = row = new Array(gridWidth);
+  var gridRow = grid[y];
+  var perlinRow = perlinNoise[y];
   for (var x = 0; x < gridWidth; ++x) {
-    row[x] = Math.floor(Math.random() * colors.length);
+    // just in case the weights don't add up to 1
+    gridRow[x] = landType.safe;
+    for (var i = 0; i < terrainThresholds.length; ++i) {
+      if (perlinRow[x] < terrainThresholds[i].threshold) {
+        gridRow[x] = terrainThresholds[i].terrain;
+        break;
+      }
+    }
   }
 }
 
@@ -73,8 +147,89 @@ function draw() {
   for (var y = 0; y < gridHeight; ++y) {
     var row = grid[y];
     for (var x = 0; x < gridWidth; ++x) {
-      context.fillStyle = colors[row[x]];
+      context.fillStyle = row[x].color;
       context.fillRect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
     }
   }
 }
+
+function generatePerlinNoise(width, height, options) {
+  options = options || {};
+  var octaveCount = options.octaveCount || 8;
+  var amplitude = options.amplitude || 0.5;
+  var persistence = options.persistence || 0.5;
+  var whiteNoise = generateWhiteNoise(gridWidth, gridHeight);
+
+  var smoothNoiseList = new Array(octaveCount);
+  var i, y, x, row;
+  for (i = 0; i < octaveCount; ++i) {
+    smoothNoiseList[i] = generateSmoothNoise(i);
+  }
+  var perlinNoise = createArray(width, height);
+  var totalAmplitude = 0;
+  // blend noise together
+  for (i = octaveCount - 1; i >= 0; --i) {
+    amplitude *= persistence;
+    totalAmplitude += amplitude;
+
+    for (y = 0; y < height; ++y) {
+      for (x = 0; x < width; ++x) {
+        perlinNoise[y][x] = perlinNoise[y][x] || 0;
+        perlinNoise[y][x] += smoothNoiseList[i][y][x] * amplitude;
+      }
+    }
+  }
+  // normalization
+  for (y = 0; y < height; ++y) {
+    for (x = 0; x < width; ++x) {
+      perlinNoise[y][x] /= totalAmplitude;
+    }
+  }
+  return perlinNoise;
+  function generateSmoothNoise(octave) {
+    var noise = createArray(width, height);
+    var samplePeriod = Math.pow(2, octave);
+    var sampleFrequency = 1 / samplePeriod;
+    for (var y = 0; y < height; ++y) {
+      var row = noise[y];
+      var sampleY0 = Math.floor(y / samplePeriod) * samplePeriod;
+      var sampleY1 = (sampleY0 + samplePeriod) % height;
+      var vertBlend = (y - sampleY0) * sampleFrequency;
+      for (var x = 0; x < width; ++x) {
+        var sampleX0 = Math.floor(x / samplePeriod) * samplePeriod;
+        var sampleX1 = (sampleX0 + samplePeriod) % width;
+        var horizBlend = (x - sampleX0) * sampleFrequency;
+
+        // blend top two corners
+        var top = interpolate(whiteNoise[sampleY0][sampleX0], whiteNoise[sampleY1][sampleX0], vertBlend);
+        // blend bottom two corners
+        var bottom = interpolate(whiteNoise[sampleY0][sampleX1], whiteNoise[sampleY1][sampleX1], vertBlend);
+        // final blend
+        row[x] = interpolate(top, bottom, horizBlend);
+      }
+    }
+    return noise;
+  }
+  function generateWhiteNoise() {
+    var noise = createArray(width, height);
+    for (var y = 0; y < height; ++y) {
+      var row = noise[y];
+      for (var x = 0; x < width; ++x) {
+        row[x] = Math.random();
+      }
+    }
+    return noise;
+  }
+  function interpolate(x0, x1, alpha) {
+    return x0 * (1 - alpha) + alpha * x1;
+  }
+}
+function createArray(w, h) {
+  var arr = new Array(h);
+  for (var y = 0; y < h; ++y) {
+    arr[y] = new Array(w);
+  }
+  return arr;
+}
+
+})();
